913fb91ebcde82764d6eed40a4d061c01e97f5a6
xing666999 <1845673805@qq.com>
2026-09-20 17:44:02 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+/* 软考中级学习平台 - Service Worker（离线缓存） */
+var CACHE = 'rkp-cache-v3';
+var ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './icon-src.jpg'];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function (c) { return c.addAll(ASSETS); }).then(function () { return self.skipWaiting(); })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.filter(function (k) { return k !== CACHE; }).map(function (k) { return caches.delete(k); }));
+    }).then(function () { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(function (r) {
+      if (r) return r;
+      return fetch(e.request).then(function (res) {
+        if (res && res.ok && new URL(e.request.url).origin === location.origin) {
+          var copy = res.clone();
+          caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
+        }
+        return res;
+      }).catch(function () {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
